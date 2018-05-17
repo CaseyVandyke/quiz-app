e3007a1c83b458b97bec02ed1e1d0d7a313858c6
Cryosith <caseyvandyke@ymail.com>
2018-05-16 22:11:43 -0600

    initial

--- content.js
+++ content.js
@@ -1,58 +1,109 @@
 const answers = {
-  correct: 'Thats correct!',
-  incorrect: 'Sorry thats incorrect!'
+    correct: 'Nice job Space Man!',
+    incorrect: 'Houston we have a problem!'
 };
 
 const quiz = {
-  questions: [
-      {
-    question: 'Which planet has the most volcanoes?',
-    a: 'Venus',
-    b: 'Jupiter',
-    c: 'Uranus',
-    d: 'Mars',
-    answer: 'a',
-    img: '',
-    explanation: ''
+    questions: [
+        {
+            question: 'Which planet has the most volcanoes?',
+            a: 'Venus',
+            b: 'Jupiter',
+            c: 'Uranus',
+            d: 'Mars',
+            answer: 'a',
+            img: '',
+            explanation: ''
   },
-  {  
-    question: 'Which planets dont have moons?',
-    a: 'Earth and Jupiter',
-    b: 'Mars and Neptune',
-    c: 'Mercury and Venus',
-    d: 'Uranus and Saturn',
-    answer: 'c',
-    img: '',
-    explanation: ''
+        {
+            question: 'Which planets dont have moons?',
+            a: 'Earth and Jupiter',
+            b: 'Mars and Neptune',
+            c: 'Mercury and Venus',
+            d: 'Uranus and Saturn',
+            answer: 'c',
+            img: '',
+            explanation: ''
   },
-  {  
-    question: 'How much larger is the Sun than the Earth?',
-    a: '300,000 times larger',
-    b: '400,000 times larger',
-    c: '100,000 times larger',
-    d: 'Twice and large',
-    answer: 'a',
-    img: '',
-    explanation: ''
+        {
+            question: 'How much larger is the Sun than the Earth?',
+            a: '300,000 times larger',
+            b: '400,000 times larger',
+            c: '100,000 times larger',
+            d: 'Twice and large',
+            answer: 'a',
+            img: '',
+            explanation: ''
   },
-  {  
-    question: 'A person who weighs 200 pounds on earth, what would he weigh on the surface of Mars?',
-    a: '83 lbs',
-    b: '45 lbs',
-    c: '76 lbs',
-    d: '640 lbs',
-    answer: 'c',
-    img: '',
-    explanation: ''
+        {
+            question: 'A person who weighs 200 pounds on earth, what would he weigh on the surface of Mars?',
+            a: '83 lbs',
+            b: '45 lbs',
+            c: '76 lbs',
+            d: '640 lbs',
+            answer: 'c',
+            img: '',
+            explanation: ''
   },
-  {  
-    question: 'In what year will Halley’s Comet will be visible from Earth again?',
-    a: '2078',
-    b: '2061',
-    c: '2405',
-    d: '2130',
-    answer: 'b',
-    img: '',
-    explanation: ''
+        {
+            question: 'What is the surface temperature of Venus?',
+            a: '180 degrees celsius',
+            b: 'Over 450 degrees celsius',
+            c: 'Below zero',
+            d: 'Comparable to earth',
+            answer: 'b',
+            img: '',
+            explanation: ''
+  },
+        {
+            question: 'When was the Solar System formed?',
+            a: '2 trillion years ago',
+            b: 'Around 4.6 billion years ago',
+            c: '100 million years ago',
+            d: '200,000 years ago',
+            answer: 'b',
+            img: '',
+            explanation: ''
+  },
+        {
+            question: 'Which Planet spins backwards relative to the others?',
+            a: 'Earth',
+            b: 'Saturn',
+            c: 'Pluto',
+            d: 'Venus',
+            answer: 'd',
+            img: '',
+            explanation: ''
+  },
+        {
+            question: 'When was the first man made object sent into space?',
+            a: '1947',
+            b: '1953',
+            c: '1957',
+            d: '1970',
+            answer: 'c',
+            img: '',
+            explanation: ''
+  },
+        {
+            question: 'Name Jupiter’s biggest moon:',
+            a: 'Ganymede',
+            b: 'Callisto',
+            c: 'Europa',
+            d: 'lo',
+            answer: 'c',
+            img: '',
+            explanation: ''
+  },
+
+        {
+            question: 'In what year will Halley’s Comet will be visible from Earth again?',
+            a: '2078',
+            b: '2061',
+            c: '2405',
+            d: '2130',
+            answer: 'b',
+            img: '',
+            explanation: ''
   }]
 };
--- script.js
+++ script.js
@@ -11,10 +11,11 @@ function getQuestions() {
           </ul>
            <form class="quiz-selection">
                 <h3 class="question-style">${quiz.questions[questionNumber].question}</h3>
-               <label for="a"><input name="option" class="answers" value="a" type="radio">${quiz.questions[questionNumber].a}</label><br>
-               <label for="b"><input name="option" class="answers" value="b" type="radio">${quiz.questions[questionNumber].b}</label><br>
-               <label for="c"><input name="option" class="answers" value="c" type="radio">${quiz.questions[questionNumber].c}</label><br>
-               <label for="d"><input name="option" class="answers" value="d" type="radio">${quiz.questions[questionNumber].d}</label><br>
+                <p class="in-out"></p>
+               <label for="a"><input name="option" class="answers" value="a" type="radio"> ${quiz.questions[questionNumber].a}</label><br>
+               <label for="b"><input name="option" class="answers" value="b" type="radio"> ${quiz.questions[questionNumber].b}</label><br>
+               <label for="c"><input name="option" class="answers" value="c" type="radio"> ${quiz.questions[questionNumber].c}</label><br>
+               <label for="d"><input name="option" class="answers" value="d" type="radio"> ${quiz.questions[questionNumber].d}</label><br>
            </form>
            <div class="submit-container">
                <button class="submit-button">Submit</button>
@@ -27,42 +28,52 @@ function getQuestions() {
 $('.quiz-start').on('click', '.js-start', function (event) {
     renderQuiz();
     $('.box').hide();
-});
+}); 
+
 
 //render quiz 
 function renderQuiz() {
     $('.quiz-questions').html(getQuestions);
     $('.quiz-questions').on('click', '.submit-button', function (event) {
         event.preventDefault();
+        if ($('input[name="option"]:checked').length === 0) {
+            $('.in-out').html(`<p class="in-out">Select an answer</p>`).fadeIn(1000);
+                
+        }   else {
+            
+        
         const valueHolder = $('.quiz-questions').find('.answers:checked').val();
-
-        for (let i = 0; i < quiz.questions.length; i++) {
-            if (valueHolder === quiz.questions[i].answer) {
+        
+       
+            if (valueHolder === quiz.questions[questionNumber].answer) {
                 score += 1;
-                $('.quiz-selection').replaceWith(`<div class="box">
+                $('.quiz-selection').replaceWith(`<div class="answer box">
                 <h2>${answers.correct}</h2>
                 <img class="gif-style" src="https://media.giphy.com/media/10kxE34bJPaUO4/giphy.gif">
                 <button class="next-option">Next</button>
                 </div>`);
 
             } else {
-                $('.quiz-selection').replaceWith(`<div class="box">
+                $('.quiz-selection').replaceWith(`<div class="answer box">
                 <h2>${answers.incorrect}</h2>
                 <img class="gif-style" src="https://media.giphy.com/media/3o6ZtfPqhS8PwCqp1e/giphy.gif">
                 <button class="next-option">Next</button>
                 </div>`);
 
             }
+        
+            
             questionNumber += 1;
 
             $('.next-option').on('click', function (event) {
                 $('.quiz-questions').html(getQuestions);
+                if (quiz.questions)
             });
 
             $('.submit-button').hide();
             return valueHolder;
         }
-       
     });
+    }
+
     
-};
