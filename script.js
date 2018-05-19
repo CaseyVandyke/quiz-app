daa7f7c716fbbf0e87311fb962c68a8e8888b397
Cryosith <caseyvandyke@ymail.com>
2018-05-18 22:17:49 -0600

    add functionality to button

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function getQuestions() {
     return `<section class="question-page">
        <div class="quiz-container">
           <ul class="quiz-progress">
-              <li>Questions: ${questionNumber}</li>
+              <li>Questions: ${questionNumber}/10</li>
               <li>Score: ${score}</li>
           </ul>
            <form class="quiz-selection">
@@ -26,9 +26,10 @@ function getQuestions() {
 
 //Start the quiz    
 $('.quiz-start').on('click', '.js-start', function (event) {
+    questionNumber =+ 1;
     renderQuiz();
     $('.box').hide();
-}); 
+});
 
 
 //render quiz 
@@ -37,43 +38,50 @@ function renderQuiz() {
     $('.quiz-questions').on('click', '.submit-button', function (event) {
         event.preventDefault();
         if ($('input[name="option"]:checked').length === 0) {
-            $('.in-out').html(`<p class="in-out">Select an answer</p>`).fadeIn(1000);
-                
-        }   else {
-            
-        
-        const valueHolder = $('.quiz-questions').find('.answers:checked').val();
-        
-       
+            $('.in-out').html(`<p class="in-out">Select an answer</p>`);
+
+        } else {
+
+
+            const valueHolder = $('.quiz-questions').find('.answers:checked').val();
+
+
             if (valueHolder === quiz.questions[questionNumber].answer) {
                 score += 1;
                 $('.quiz-selection').replaceWith(`<div class="answer box">
-                <h2>${answers.correct}</h2>
+                <h3>${answers.correct}</h3>
                 <img class="gif-style" src="https://media.giphy.com/media/10kxE34bJPaUO4/giphy.gif">
                 <button class="next-option">Next</button>
                 </div>`);
 
             } else {
                 $('.quiz-selection').replaceWith(`<div class="answer box">
-                <h2>${answers.incorrect}</h2>
+                <h3>${answers.incorrect}</h3>
                 <img class="gif-style" src="https://media.giphy.com/media/3o6ZtfPqhS8PwCqp1e/giphy.gif">
                 <button class="next-option">Next</button>
                 </div>`);
 
             }
-        
-            
+
+
             questionNumber += 1;
 
             $('.next-option').on('click', function (event) {
                 $('.quiz-questions').html(getQuestions);
-                if (quiz.questions)
+                
             });
 
             $('.submit-button').hide();
             return valueHolder;
         }
     });
-    }
+}
 
-    
+function endQuiz() {
+    $('.quiz-questions').append(` <div class="quiz-end">
+        <p>Your score was ${score} out of 10!</p>
+        <h2>Your score is out of this world</h2>
+        <button class="submit-button">Restart</button>
+    </div>`)
+}
+
