d301108b76b9a2a73a3b3363278e405bc44a0936
Cryosith <caseyvandyke@ymail.com>
2018-05-28 00:21:01 -0600

    fin

--- content.js
+++ content.js
@@ -1,103 +1,122 @@
-const answers = {
+const results = {
     correct: 'Nice job Space Man!',
     incorrect: 'Houston we have a problem!'
 };
 
-const quiz = {
-    questions: [
-        {
-            question: 'Which planet has the most volcanoes?',
-            a: 'Venus',
-            b: 'Jupiter',
-            c: 'Uranus',
-            d: 'Mars',
-            answer: 'a',
-            explanation: 'Venus has more volcanoes than any other planet in the solar system. Over 1600 major volcanoes or volcanic features are known, and there are many, many more smaller volcanoes.'
+const questions = [
+    {
+        question: 'Which planet has the most volcanoes?',
+        answers: [
+            'Venus',
+            'Jupiter',
+            'Uranus',
+            'Mars'
+                ],
+        answer: 'Venus',
+        explanation: 'Venus has more volcanoes than any other planet in the solar system. Over 1600 major volcanoes or volcanic features are known, and there are many, many more smaller volcanoes.'
   },
 
-        {
-            question: 'Which planets dont have moons?',
-            a: 'Earth and Jupiter',
-            b: 'Mars and Neptune',
-            c: 'Mercury and Venus',
-            d: 'Uranus and Saturn',
-            answer: 'c',
-            explanation: 'Mercury and Venus are the only planets without moons.'
+    {
+        question: 'Which planets don\'t have moons?',
+        answers: [
+            'Earth and Jupiter',
+            'Mars and Neptune',
+            'Mercury and Venus',
+            'Uranus and Saturn',
+                ],
+        answer: 'Mercury and Venus',
+        explanation: 'Mercury and Venus are the only planets without moons.'
   },
 
-        {
-            question: 'How many Earths can fit inside the Sun?',
-            a: 'About 100 Earths fit inside the sun',
-            b: 'About 5,000 Earths fit inside the sun',
-            c: 'About 10,330 Earths fit inside the sun',
-            d: 'About 1.3 million Earths fit inside the sun',
-            answer: 'd',
-            explanation: 'It would take 1.3 million Earths to fill up the Sun. That\'s a lot of Earths.'
+    {
+        question: 'How many Earths can fit inside the Sun?',
+        answers: [
+            'About 100 Earths fit inside the sun',
+            'About 5,000 Earths fit inside the sun',
+            'About 10,330 Earths fit inside the sun',
+            'About 1.3 million Earths fit inside the sun',
+                ],
+        answer: 'About 1.3 million Earths fit inside the sun',
+        explanation: 'It would take 1.3 million Earths to fill up the Sun. That\'s a lot of Earths.'
   },
 
 
-        {
-            question: 'A person who weighs 200 pounds on earth, what would he weigh on the surface of Mars?',
-            a: '83 lbs',
-            b: '45 lbs',
-            c: '76 lbs',
-            d: '640 lbs',
-            answer: 'c',
-            explanation: 'This phenomenon is known as gravity. More massive objects have a stronger pull than less massive objects. This is why you weigh more on larger, more massive planets like Jupiter and less on smaller, less massive planets like Mars. '
+    {
+        question: 'A person who weighs 200 pounds on earth, what would he weigh on the surface of Mars?',
+        answers: [
+            '83 lbs',
+            '45 lbs',
+            '76 lbs',
+            '640 lbs',
+                ],
+        answer: '76 lbs',
+        explanation: 'This phenomenon is known as gravity. More massive objects have a stronger pull than less massive objects. This is why you weigh more on larger, more massive planets like Jupiter and less on smaller, less massive planets like Mars. '
   },
-        {
-            question: 'What is the surface temperature of Venus?',
-            a: '180 degrees celsius',
-            b: 'Over 450 degrees celsius',
-            c: 'Below zero',
-            d: 'Comparable to earth',
-            answer: 'b',
-            explanation: 'Venus is so hot because it is surrounded by a very thick atmosphere which is about 100 times more massive than our atmosphere here on Earth. As sunlight passes through the atmosphere, it heats up the surface of Venus. ... The heat becomes trapped and builds up to extremely high temperatures.'
+    {
+        question: 'What is the surface temperature of Venus?',
+        answers: [
+            '180 degrees celsius',
+            'Over 450 degrees celsius',
+            'Below zero',
+            'Comparable to earth',
+                ],
+        answer: 'Over 450 degrees celsius',
+        explanation: 'Venus is so hot because it is surrounded by a very thick atmosphere which is about 100 times more massive than our atmosphere here on Earth. As sunlight passes through the atmosphere, it heats up the surface of Venus. ... The heat becomes trapped and builds up to extremely high temperatures.'
   },
-        {
-            question: 'When was the Solar System formed?',
-            a: '2 trillion years ago',
-            b: 'Around 4.6 billion years ago',
-            c: '100 million years ago',
-            d: '200,000 years ago',
-            answer: 'b',
-            explanation: 'The solar system is a pretty busy place. It\'s got all kinds of planets, moons, asteroids, and comets zipping around our Sun. But how did this busy stellar neighborhood come to be? Our story starts about 4.6 billion years ago, with a wispy cloud of stellar dust.'
+    {
+        question: 'When was the Solar System formed?',
+        answers: [
+            '2 trillion years ago',
+            'Around 4.6 billion years ago',
+            '100 million years ago',
+            '200,000 years ago',
+                ],
+        answer: 'Around 4.6 billion years ago',
+        explanation: 'The solar system is a pretty busy place. It\'s got all kinds of planets, moons, asteroids, and comets zipping around our Sun. But how did this busy stellar neighborhood come to be? Our story starts about 4.6 billion years ago, with a wispy cloud of stellar dust.'
   },
-        {
-            question: 'Which Planet spins backwards relative to the others?',
-            a: 'Earth',
-            b: 'Saturn',
-            c: 'Pluto',
-            d: 'Venus',
-            answer: 'd',
-            explanation: 'Venus spins in the opposite direction from most other planets, including Earth, so that on Venus the sun rises in the west'
+    {
+        question: 'Which Planet spins backwards relative to the others?',
+        answers: [
+            'Earth',
+            'Saturn',
+            'Pluto',
+            'Venus',
+                ],
+        answer: 'Venus',
+        explanation: 'Venus spins in the opposite direction from most other planets, including Earth, so that on Venus the sun rises in the west'
   },
-        {
-            question: 'When was the first man made object sent into space?',
-            a: '1947',
-            b: '1953',
-            c: '1942',
-            d: '1970',
-            answer: 'c',
-            explanation: 'A German rocket was the first man made object to reach space, October 3, 1942.'
+    {
+        question: 'When was the first man made object sent into space?',
+        answers: [
+            1947,
+            1953,
+            1942,
+            1970,
+                ],
+        answer: 1942,
+        explanation: 'A German rocket was the first man made object to reach space, October 3, 1942.'
   },
-        {
-            question: 'Name Jupiter’s biggest moon:',
-            a: 'Ganymede',
-            b: 'Callisto',
-            c: 'Europa',
-            d: 'lo',
-            answer: 'a',
-            explanation: 'Ganymede exceeds even the planet Mercury possessing a diameter of 3,274 miles'
+    {
+        question: 'Name Jupiter’s biggest moon:',
+        answers: [
+            'Ganymede',
+            'Callisto',
+            'Europa',
+            'lo',
+                ],
+        answer: 'Ganymede',
+        explanation: 'Ganymede exceeds even the planet Mercury possessing a diameter of 3,274 miles'
   },
 
-        {
-            question: 'In what year will Halley’s Comet will be visible from Earth again?',
-            a: '2078',
-            b: '2061',
-            c: '2405',
-            d: '2130',
-            answer: 'b',
-            explanation: 'Halley is the only known short-period comet that is regularly visible to the naked eye from Earth, and the only naked-eye comet that might appear twice in a human lifetime. Halley last appeared in the inner parts of the Solar System in 1986 and will next appear in mid-2061.'
-  }]
-};
+    {
+        question: 'In what year will Halley’s Comet be visible from Earth again?',
+        answers: [
+            2078,
+            2061,
+            2405,
+            2130,
+                ],
+        answer: 2061,
+        explanation: 'Halley is the only known short-period comet that is regularly visible to the naked eye from Earth, and the only naked-eye comet that might appear twice in a human lifetime. Halley last appeared in the inner parts of the Solar System in 1986 and will next appear in mid-2061.'
+
+        }];
--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 let questionNumber = 0;
 let score = 0;
+let newHTML = "";
+
+
 
 //get quiz questions 
 function getQuestions() {
-    return `<section class="question-page">
+    newHTML = "";
+    for (let i = 0; i < questions[questionNumber].answers.length; i++) {
+
+        newHTML += `<label><input name="option" class="answers" data-answer="${questions[questionNumber].answers[i]}" type="radio"> ${questions[questionNumber].answers[i]} </label><br>`;
+    }
+    return `<fieldset><legend>Space Quiz</legend><section class="question-page">
        <div class="quiz-container">
           <ul class="quiz-progress">
               <li>Questions: ${questionNumber+1}/10</li>
-              <li>Score: ${score}</li>
+              <li id="score">Score: ${score}</li>
           </ul>
-           <form class="quiz-form">
-                <h3 class="question-style">${quiz.questions[questionNumber].question}</h3>
+           <div class="quiz-form">
+                <h3 class="question-style">${questions[questionNumber].question}</h3>
                 <p class="in-out"></p>
-               <label for="a"><input name="option" class="answers" value="a" type="radio"> ${quiz.questions[questionNumber].a}</label><br>
-               <label for="b"><input name="option" class="answers" value="b" type="radio"> ${quiz.questions[questionNumber].b}</label><br>
-               <label for="c"><input name="option" class="answers" value="c" type="radio"> ${quiz.questions[questionNumber].c}</label><br>
-               <label for="d"><input name="option" class="answers" value="d" type="radio"> ${quiz.questions[questionNumber].d}</label><br>
-               <div class="submit-container">
-               <button class="appear js-restart">Restart</button>
+                ${newHTML}
+           </div>
+           <div class="submit-container">
                <button class="submit-button">Submit</button>
-               </div>
-           </form>
-            
+           </div>
            
        </div>
-        <div class="gif-container">
-            <img class="gif-small" src="https://media.giphy.com/media/3oKIPtjElfqwMOTbH2/source.gif">
-        </div>
-   </section>`
+   </section> </fieldset>`
 }
 
 //Start the quiz    
@@ -40,18 +40,38 @@ $('.quiz-start').on('click', '.js-start', function (event) {
 //render quiz 
 function renderQuiz() {
     $('.quiz-questions').html(getQuestions());
-    $('.quiz-questions').on('click', '.submit-button', function (event) {
+    $('.quiz-container').submit(function (event) {
         event.preventDefault();
+
+
+
         if ($('input[name="option"]:checked').length === 0) {
             $('.in-out').html(`<p class="in-out">Select an answer</p>`);
 
         } else {
+            $('.submit-button').hide();
 
+            const valueHolder = $('.quiz-container').find('input[name="option"]:checked').data('answer');
 
-            const valueHolder = $('.quiz-questions').find('.answers:checked').val();
-
-            if (questionNumber === quiz.questions.length - 1 && valueHolder === quiz.questions[questionNumber].answer) {
+            if (valueHolder === questions[questionNumber].answer) {
                 score += 1;
+                $('.quiz-form').html(`<div class="answer box">
+                <h3>${results.correct}</h3>
+                <p class="fact-style">${questions[questionNumber].explanation}</p>
+                <img class="gif-style" src="https://media.giphy.com/media/l4KhQo2MESJkc6QbS/giphy.gif">
+                <button class="next-option">Next</button>
+                </div>`);
+            } else {
+
+                $('.quiz-form').html(`<div class="answer box">
+                <h3>${results.incorrect}</h3>
+                <p class="fact-style">${questions[questionNumber].explanation}</p>
+                <img class="gif-style" src="https://media.giphy.com/media/3o6ZtfPqhS8PwCqp1e/giphy.gif">
+                <button class="next-option">Next</button>
+                </div>`);
+            };
+            if (questionNumber === questions.length - 1) {
+
                 if (score <= 6) {
                     $('.quiz-form').html(`<div class="answer box">
             <h3>Get out of here with that score!</h3>
@@ -60,7 +80,7 @@ function renderQuiz() {
             <p class="final-score">Give it another shot?</p>
             <button class="appear js-restart">Restart</button>
             </div>`);
-                }   else if (score >= 7) {
+                } else if (score >= 7) {
                     $('.quiz-form').html(`<div class="answer box">
             <h3>See you, Space Cowboy!</h3>
             <p class="final-score">You got ${score} out of 10!</p>
@@ -69,38 +89,29 @@ function renderQuiz() {
             <button class="appear js-restart">Restart</button>
             </div>`);
                 }
-                
+
                 $('.js-restart').show();
                 $('.quiz-form').on('click', '.js-restart', function (event) {
+                    score = 0;
+                    questionNumber = 0;
                     renderQuiz();
 
                 });
-            } else if (valueHolder === quiz.questions[questionNumber].answer) {
-                score += 1;
-                $('.quiz-form').html(`<div class="answer box">
-                <h3>${answers.correct}</h3>
-                <p class="fact-style">${quiz.questions[questionNumber].explanation}</p>
-                <img class="gif-style" src="https://media.giphy.com/media/l4KhQo2MESJkc6QbS/giphy.gif">
-                <button class="next-option">Next</button>
-                </div>`);
-                //https://media.giphy.com/media/xT8qBhrlNooHBYR9f2/giphy.gif
-            } else {
+            };
 
-                $('.quiz-form').html(`<div class="answer box">
-                <h3>${answers.incorrect}</h3>
-                <p class="fact-style">${quiz.questions[questionNumber].explanation}</p>
-                <img class="gif-style" src="https://media.giphy.com/media/3o6ZtfPqhS8PwCqp1e/giphy.gif">
-                <button class="next-option">Next</button>
-                </div>`);
-            }
 
+
+            $('.quiz-questions').find("li#score").html(`<li id="score">Score: ${score}</li>`);
             questionNumber += 1;
 
             $('.next-option').on('click', function (event) {
+
                 $('.quiz-questions').html(getQuestions);
+
+
             });
 
             return valueHolder;
-        }
-    });
-};
+        };
+    })
+}
